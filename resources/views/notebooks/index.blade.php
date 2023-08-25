9333e503531914f81a8ee4e2f8e038f799541727
Scott Knoll <scknoll88@gmail.com>
2023-08-24 21:31:40 -0500

    notebook index

---
+++ resources/views/notebooks/index.blade.php
@@ -0,0 +1,96 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex items-center justify-between">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Notebooks
+            </h2>
+            <div class="inline-flex rounded-md shadow-sm isolate">
+                <x-button href="/notes/create" styles="indigo">
+                    Create Notebook
+                </x-button>
+            </div>
+        </div>
+    </x-slot>
+    <x-container>
+        <div class="flow-root mt-8">
+            <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <table class="min-w-full divide-y divide-gray-300">
+                        <thead>
+                            <tr>
+                                <th scope="col"
+                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                                    <a href="#" class="inline-flex ml-2 group">
+                                        Book
+                                        <!-- Active: "bg-gray-200 text-gray-900 group-hover:bg-gray-300", Not Active: "invisible text-gray-400 group-hover:visible group-focus:visible" -->
+                                        <span
+                                            class="flex-none invisible ml-2 text-gray-400 rounded group-hover:visible group-focus:visible">
+                                            <svg class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor"
+                                                aria-hidden="true">
+                                                <path fill-rule="evenodd"
+                                                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </span>
+                                    </a>
+                                </th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    <a href="#" class="inline-flex group">
+                                        Created By
+                                        <!-- Active: "bg-gray-200 text-gray-900 group-hover:bg-gray-300", Not Active: "invisible text-gray-400 group-hover:visible group-focus:visible" -->
+                                        <span
+                                            class="flex-none ml-2 text-gray-900 bg-gray-100 rounded group-hover:bg-gray-200">
+                                            <svg class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor"
+                                                aria-hidden="true">
+                                                <path fill-rule="evenodd"
+                                                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </span>
+                                    </a>
+                                </th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    <a href="#" class="inline-flex group">
+                                        Updated
+                                        <!-- Active: "bg-gray-200 text-gray-900 group-hover:bg-gray-300", Not Active: "invisible text-gray-400 group-hover:visible group-focus:visible" -->
+                                        <span
+                                            class="flex-none invisible ml-2 text-gray-400 rounded group-hover:visible group-focus:visible">
+                                            <svg class="flex-none invisible w-5 h-5 ml-2 text-gray-400 rounded group-hover:visible group-focus:visible"
+                                                viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd"
+                                                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </span>
+                                    </a>
+                                </th>
+                                <th scope="col" class="relative py-3.5 pl-3 pr-0">
+                                    <span class="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            @foreach ($notebooks as $notebook)
+                                <tr>
+                                    <td
+                                        class="py-4 pl-4 pr-3 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-2">
+                                        {{ $notebook->name }}</td>
+                                    <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">
+                                        {{ $notebook->user->name }}
+                                    </td>
+                                    <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">
+                                        {{ $notebook->updated_at->format('M d, Y') }}
+                                    </td>
+                                    <td class="relative py-4 pl-3 pr-4 text-sm text-right whitespace-nowrap sm:pr-2">
+                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span
+                                                class="sr-only">, Lindsay Walton</span></a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </x-container>
+</x-app-layout>
