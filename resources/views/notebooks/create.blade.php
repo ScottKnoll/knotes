c5af1de68d648a8375ea0e3c0909d3a75e7f619f
Scott Knoll <scknoll88@gmail.com>
2024-02-03 22:15:57 -0600

    notebook crud updates

--- resources/views/notebooks/create.blade.php
+++ resources/views/notebooks/create.blade.php
@@ -1,31 +1,27 @@
 <x-layout>
-    <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            Create Notebook
-        </h2>
-    </x-slot>
+    <h2 class="text-xl font-semibold leading-tight text-gray-800">
+        Create Notebook
+    </h2>
 
-    <x-container>
-        <div class="py-4 mx-auto">
-            <x-validation-errors />
-            <form action="/notebooks" method="post">
-                @csrf
-                <div class="grid grid-cols-1 mt-4 gap-x-6 gap-y-4">
-                    <div>
-                        <x-label for="name" class="mb-1">Name</x-label>
-                        <x-input type="text" name="name" :value="old('name')" />
-                    </div>
-                    <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+    <div class="mx-auto py-4">
+        <x-validation-errors />
+        <form action="/notebooks" method="post">
+            @csrf
+            <div class="mt-4 grid grid-cols-1 gap-x-6 gap-y-4">
+                <div>
+                    <x-label for="name" class="mb-1">Name</x-label>
+                    <x-input type="text" name="name" :value="old('name')" />
                 </div>
-                <div class="sm:pt-5">
-                    <div class="flex justify-end gap-x-3">
-                        <x-button href="/notebooks" class="rounded-md hover:bg-gray-50">
-                            Cancel
-                        </x-button>
-                        <x-button type="submit" styles="indigo">
-                            Create</x-button>
-                    </div>
+                <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+            </div>
+            <div class="sm:pt-5">
+                <div class="flex justify-end gap-x-3">
+                    <x-button href="/notebooks" class="rounded-md hover:bg-gray-50">
+                        Cancel
+                    </x-button>
+                    <x-button type="submit" styles="indigo">
+                        Create</x-button>
                 </div>
-            </form>
-    </x-container>
+            </div>
+        </form>
 </x-layout>
--- resources/views/notebooks/edit.blade.php
+++ resources/views/notebooks/edit.blade.php
@@ -1,32 +1,28 @@
 <x-layout>
-    <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            Edit Notebook
-        </h2>
-    </x-slot>
+    <h2 class="text-xl font-semibold leading-tight text-gray-800">
+        Edit Notebook
+    </h2>
 
-    <x-container>
-        <div class="mx-auto py-4">
-            <x-validation-errors />
-            <form action="/notebooks/{{ $notebook->id }}" method="post">
-                @csrf
-                @method('patch')
-                <div class="mt-4 grid grid-cols-1 gap-x-6 gap-y-4">
-                    <div>
-                        <x-label for="name" class="mb-1">Name</x-label>
-                        <x-input type="text" name="name" :value="old('name', $notebook->name)" />
-                    </div>
-                    <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+    <div class="mx-auto py-4">
+        <x-validation-errors />
+        <form action="/notebooks/{{ $notebook->id }}" method="post">
+            @csrf
+            @method('patch')
+            <div class="mt-4 grid grid-cols-1 gap-x-6 gap-y-4">
+                <div>
+                    <x-label for="name" class="mb-1">Name</x-label>
+                    <x-input type="text" name="name" :value="old('name', $notebook->name)" />
                 </div>
-                <div class="sm:pt-5">
-                    <div class="flex justify-end gap-x-3">
-                        <x-button href="/notebooks/{{ $notebook->id }}" class="rounded-md hover:bg-gray-50">
-                            Cancel
-                        </x-button>
-                        <x-button type="submit" styles="indigo">
-                            Update</x-button>
-                    </div>
+                <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+            </div>
+            <div class="sm:pt-5">
+                <div class="flex justify-end gap-x-3">
+                    <x-button href="/notebooks/{{ $notebook->id }}" class="rounded-md hover:bg-gray-50">
+                        Cancel
+                    </x-button>
+                    <x-button type="submit" styles="indigo">
+                        Update</x-button>
                 </div>
-            </form>
-    </x-container>
+            </div>
+        </form>
 </x-layout>
