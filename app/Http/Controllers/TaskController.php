6fd68a33a9f4b4abc7f8575f327a58ff5290e1e5
Scott Knoll <scknoll88@gmail.com>
2024-02-21 22:04:54 -0600

    tasks wip

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Note;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
-    //
+    public function store(Note $note)
+    {
+        $validated = request()->validate([
+            'title' => 'required|max:255',
+            'status' => 'nullable|date',
+            'due_date' => 'required|boolean',
+        ]);
+
+        $note->tasks()->create($validated);
+
+        session()->flash('alert_message', 'Task created successfully.');
+
+        return redirect()->route('notes.index');
+    }
+
+    public function update(Note $note)
+    {
+        $validated = request()->validate([
+            'title' => 'required|max:255',
+            'status' => 'nullable|date',
+            'due_date' => 'required|boolean',
+        ]);
+
+        $note->tasks()->update($validated);
+
+        session()->flash('alert_message', 'Task updated successfully.');
+
+        return redirect()->route('notes.show', ['note' => $note->id]);
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        session()->flash('alert_message', 'Task deleted successfully.');
+
+        return redirect()->route('notes.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,11 @@
 <?php
 
-use App\Http\Controllers\NoteAssignmentController;
-use App\Http\Controllers\NotebookController;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\NotebookController;
+use App\Http\Controllers\NoteAssignmentController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,6 +18,8 @@ Route::get('/dashboard', function () {
 Route::resource('notes', NoteController::class)->except(['show']);
 Route::resource('notebooks', NotebookController::class);
 Route::resource('assigned-notes', NoteAssignmentController::class)->only(['store', 'destroy']);
+Route::resource('tasks', TaskController::class)->only(['store', 'update', 'destroy']);
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
